0665ef16ac6eb9fca97201f50fb8b3e626053aa7
AmrAnter44 <137618860+AmrAnter44@users.noreply.github.com>
2025-11-11 02:20:33 +0200

    asd

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,18 +6,18 @@ export default function Footer() {
       <footer className="text-white flex flex-row justify-between bg-red-600 p-0 m-0 mt-auto">
         <div className="flex flex-row">
           <a
-            href="https://www.instagram.com/x_ggym?igsh=MWEwdjV0eXk0MnBseQ=="
+            href="https://www.instagram.com/eaglegym2024"
             className="text-white p-2 lg:p-4"
           >
             <i className="p-1 fa-brands fa-instagram text-2xl text-white mt-1"></i>
           </a>
-          <a href="https://wa.link/zyhpxd" className="text-white p-2 lg:p-4">
+          <a href="https://wa.me/201122010294" className="text-white p-2 lg:p-4">
             <i className="p-1 fa-brands fa-whatsapp text-2xl text-white mt-1"></i>
           </a>
-          <a href="https://www.facebook.com/TOWN411" className="text-white p-2 lg:p-4">
+          <a href="https://www.facebook.com/profile.php?id=61554637528211" className="text-white p-2 lg:p-4">
             <i className="p-1 fa-brands fa-facebook text-2xl text-white mt-1"></i>
           </a>
-          <a href="https://maps.app.goo.gl/goVtwSXvxrkmpzzPA" className="text-white p-2 lg:p-4">
+          <a href="https://www.google.com/maps/place/eagle+gym+%D8%A7%D9%84%D9%81%D8%B3%D8%B7%D8%A7%D8%B7%E2%80%AD/data=!4m2!3m1!1s0x145847004519ae1f:0xd7eaec49d4769777?sa=X&ved=1t:242&ictx=111" className="text-white p-2 lg:p-4">
             <i className="fa-solid fa-location-dot text-2xl text-white mt-1 p-1"></i>
           </a>
         </div>
@@ -37,7 +37,7 @@ export default function Footer() {
 
 
 <iframe 
-  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d55268.9852979517!2d31.184628664368432!3d30.027917688996002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x145847fba96bcafb%3A0x470868e04054808c!2sX-GYM!5e0!3m2!1sen!2seg!4v1757829847394!5m2!1sen!2seg" 
+src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3455.060212663338!2d31.252080799999998!3d30.006427400000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x145847004519ae1f%3A0xd7eaec49d4769777!2sEAGLE%20GYM!5e0!3m2!1sen!2seg!4v1762820295028!5m2!1sen!2seg"
   width="100%" 
   height="450" 
   style={{ border: 0 }} 
@@ -45,6 +45,7 @@ export default function Footer() {
   loading="lazy" 
   referrerPolicy="no-referrer-when-downgrade"
 ></iframe>
+
     </>
   );
 }
